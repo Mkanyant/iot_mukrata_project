abdbc6f2b7e124eb693160263d9e1ca0c67801cc
Mkanyant <s6519410049@sau.ac.th>
2025-03-19 11:46:30 +0700

    end all

--- lib/views/about_ui.dart
+++ lib/views/about_ui.dart
@@ -11,7 +11,74 @@ class _AboutUiState extends State<AboutUi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 60.0,
+            ),
+            Image.asset(
+              'assets/images/saulogo.png',
+              width: 250.0,
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            Text(
+              'Tech SAU BUFFET',
+              style: TextStyle(
+                color: Colors.deepOrange,
+                fontSize: 30.0,
+              ),
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            Text(
+              'แอปพลิเคชันร้านหมูกระทะ',
+              style: TextStyle(
+                color: Colors.grey[700],
+                fontSize: 20.0,
+              ),
+            ),
+            Text(
+              'เพื่อคนไทย',
+              style: TextStyle(
+                color: Colors.grey[700],
+                fontSize: 20.0,
+              ),
+            ),
+            Text(
+              'โดยเด็กไทย',
+              style: TextStyle(
+                color: Colors.grey[700],
+                fontSize: 20.0,
+              ),
+            ),
+            Text(
+              'สนใจแอปพลิเคชันติดต่อ',
+              style: TextStyle(
+                color: Colors.grey[700],
+                fontSize: 20.0,
+              ),
+            ),
+            Text(
+              'เด็กไอที SAU',
+              style: TextStyle(
+                color: Colors.grey[700],
+                fontSize: 35.0,
+              ),
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            Image.asset(
+              'assets/images/sauqr.png',
+              width: 130.0,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
